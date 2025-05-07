db44d3766d646be07a5c2ce1fbc28e79fb515c95
sylvainshan <sylvain.teff@gmail.com>
2025-05-07 12:28:59 +0200

    Update utils.py set_seed function

--- src/utils.py
+++ src/utils.py
@@ -845,6 +845,8 @@ def set_seed(seed):
         torch.cuda.manual_seed(seed)
         torch.cuda.manual_seed_all(seed) 
     torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+    os.environ['PYTHONHASHSEED'] = str(seed)
 
 def worker_init_fn(worker_id):
     """Sets the seed for DataLoader workers."""
